225ae6f1cd21b9cd1fdda8ff51de7cedf0fa09bb
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-05 14:26:08 +0100

    enable entra login only

---
+++ entra_admin.tf
@@ -0,0 +1,6 @@
+resource "azurerm_mssql_active_directory_administrator" "sql_ad_admin" {
+  server_id            = azurerm_mssql_server.sql_server.id
+  login               = "MyAADAdmin"  # This is just a display name
+  object_id           = data.azuread_client_config.current.object_id
+  tenant_id           = data.azuread_client_config.current.tenant_id
+}
--- sql_server.tf
+++ sql_server.tf
@@ -4,8 +4,9 @@ resource "azurerm_mssql_server" "sql_server" {
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = var.admin_login
-  administrator_login_password = var.admin_password
+  # administrator_login          = var.admin_login
+  # administrator_login_password = var.admin_password
+  
 
   # Security Settings
   minimum_tls_version = "1.2"
